fee9dad4ef0befe12bc5536235985bc6a4ab0fff
Ekaterina Mironenko <68422874+FeelinnGlad@users.noreply.github.com>
2021-03-31 11:15:41 +0300

    Update Task2.js

--- Language/Functions/Task2.js
+++ Language/Functions/Task2.js
@@ -4,13 +4,6 @@ The previous chapter introduced the standard function Math.min that returns its 
 We can do that ourselves now.
 
 Write a function min that takes two arguments and returns their minimum.
-
-// Your code here.
-
-console.log(min(0, 10));
-// → 0
-console.log(min(0, -10));
-// → -10
 */
 
 function min (_1, _2) {
