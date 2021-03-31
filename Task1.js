7d052258dc9c2319443204b2b384afdb0fb3d588
Ekaterina Mironenko <68422874+FeelinnGlad@users.noreply.github.com>
2021-03-31 11:00:33 +0300

    Create Task1.js

---
+++ Task1.js
@@ -0,0 +1,27 @@
+/*
+#
+##
+###
+####
+#####
+######
+#######
+Separate new lines with '\n' symbol, so the output string looks like this:
+
+"#\n##\n###\n####\n#####\n######\n#######\n"
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+var abc = "abc";
+console.log(abc.length);
+// → 3
+*\
+
+function forLoop () {
+  let line = '';
+  for (let delta = '#'; delta.length <= 7; delta += '#') {
+    line += `${delta}\n`;
+  }
+  return line;
+}
+
+forLoop();
