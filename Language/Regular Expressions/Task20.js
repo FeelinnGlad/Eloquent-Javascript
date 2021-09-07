ff3a00b5cee81629d596999a792df57f5ff392f2
Ekaterina Mironenko <68422874+FeelinnGlad@users.noreply.github.com>
2021-09-07 19:14:57 +0300

    Create Task20.js

---
+++ Language/Regular Expressions/Task20.js
@@ -0,0 +1,24 @@
+// Filled with regular expression
+var number = /^([-+])?((\d+(\.\d+)?e([-+])?\d+)|(\d+\.?(\d+)?)|(\.\d+))$/gi;
+
+// Tests:
+function doTestNumber() {
+    // Values to be accepted:
+    ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
+        "1e+12"].forEach(function (s) {
+        if (!number.test(s)) {
+            console.log("Failed to match '" + s + "'");
+            return false;
+        }
+    });
+    // Irrelevant values:
+    ["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
+        "."].forEach(function (s) {
+        if (number.test(s)) {
+            console.log("Incorrectly accepted '" + s + "'");
+            return false;
+        }
+    });
+}
+
+console.log(doTestNumber());
